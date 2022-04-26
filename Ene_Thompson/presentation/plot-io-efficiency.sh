51cb19e84cc9aa73a8b806d8d552af8eb46f44e0
Ross Thompson <ross1728@gmail.com>
2022-04-26 00:06:34 -0500

    Fixed typos.

--- Ene_Thompson/presentation/plot-io-efficiency.sh
+++ Ene_Thompson/presentation/plot-io-efficiency.sh
@@ -1,9 +1,9 @@
 set terminal postscript portrait enhanced color dashed lw 1 "DejaVuSans" 12
 set size 1, 0.5
 set output "io-eff.ps"
-set title "Efficiency vs Vin"
+set title "Efficiency vs Iout"
 set ylabel "Efficiency (%)"
-set xlabel "Vin (V)"
+set xlabel "Iout (A)"
 unset key
 plot "io-data.txt" using 9:8 with lines
  
--- Ene_Thompson/presentation/plot-io-ripple.sh
+++ Ene_Thompson/presentation/plot-io-ripple.sh
@@ -1,9 +1,9 @@
 set terminal postscript portrait enhanced color dashed lw 1 "DejaVuSans" 12
 set size 1, 0.5
 set output "io-ripple.ps"
-set title "Ripple vs Vin"
+set title "Ripple vs Iout"
 set ylabel "Ripple (mV)"
-set xlabel "Vin (V)"
+set xlabel "Iout (A)"
 unset key
 plot "io-data.txt" using 9:5 with lines
  
